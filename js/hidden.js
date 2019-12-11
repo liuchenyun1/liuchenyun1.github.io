1a4afdb371397c6c09845b3e492146ff9e5d4e47
liuchenyun1 <liu_chenyun@outlook.com>
2019-12-11 17:49:58 +0800

    modified:   css/css.css 	modified:   game/2048/index.html 	modified:   game/2048/style/main.css 	modified:   game/index.html 	modified:   game/paint/css/site.css 	modified:   index.html 	modified:   js/hidden.js

--- js/hidden.js
+++ js/hidden.js
@@ -1,9 +1,15 @@
 TITLE = document.getElementsByTagName("title")[0].innerText;
-if (document.hidden) {
-  document.getElementsByTagName("title")[0].innerText = '网页崩溃了';
-  while (document.hidden) {}
-  document.getElementsByTagName("title")[0].innerText = '诶 又好了QwQ';
-  setTimeout(function () {
+last = false;
+document.addEventListener("visibilitychange", function () {
+  console.log(document.hidden, " ", last);
+  if (document.hidden) {
+    document.getElementsByTagName("title")[0].innerText = '网页崩溃了';
+  } else {
+    if (last) {
+      document.getElementsByTagName("title")[0].innerText = '又好了QwQ';
+      setTimeout(function () {}, 1000);
+    }
     document.getElementsByTagName("title")[0].innerText = TITLE;
-  }, 1000);
-}
+  }
+  last = document.hidden;
+});
