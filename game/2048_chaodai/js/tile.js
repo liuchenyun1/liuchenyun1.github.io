a1d4519ffd7d652f519f52dd6da816ff080bbe12
liuchenyun1 <liu_chenyun@outlook.com>
2019-12-31 19:55:28 +0800

    new file:   game/2048_5x5/favicon.ico 	new file:   game/2048_5x5/index.html 	new file:   game/2048_5x5/js/animframe_polyfill.js 	new file:   game/2048_5x5/js/application.js 	new file:   game/2048_5x5/js/bind_polyfill.js 	new file:   game/2048_5x5/js/classlist_polyfill.js 	new file:   game/2048_5x5/js/game_manager.js 	new file:   game/2048_5x5/js/grid.js 	new file:   game/2048_5x5/js/html_actuator.js 	new file:   game/2048_5x5/js/keyboard_input_manager.js 	new file:   game/2048_5x5/js/local_storage_manager.js 	new file:   game/2048_5x5/js/tile.js 	new file:   game/2048_5x5/meta/apple-touch-icon.png 	new file:   game/2048_5x5/meta/apple-touch-startup-image-640x1096.png 	new file:   game/2048_5x5/meta/apple-touch-startup-image-640x920.png 	new file:   game/2048_5x5/meta/icon_bitcoin.svg 	new file:   game/2048_5x5/meta/icon_pp.svg 	new file:   game/2048_5x5/style/main.css 	new file:   game/2048_chaodai/favicon.ico 	new file:   game/2048_chaodai/index.html 	new file:   game/2048_chaodai/js/animframe_polyfill.js 	new file:   game/2048_chaodai/js/application.js 	new file:   game/2048_chaodai/js/bind_polyfill.js 	new file:   game/2048_chaodai/js/classlist_polyfill.js 	new file:   game/2048_chaodai/js/game_manager.js 	new file:   game/2048_chaodai/js/grid.js 	new file:   game/2048_chaodai/js/html_actuator.js 	new file:   game/2048_chaodai/js/keyboard_input_manager.js 	new file:   game/2048_chaodai/js/local_storage_manager.js 	new file:   game/2048_chaodai/js/tile.js 	new file:   game/2048_chaodai/meta/apple-touch-icon.png 	new file:   game/2048_chaodai/meta/apple-touch-startup-image-640x1096.png 	new file:   game/2048_chaodai/meta/apple-touch-startup-image-640x920.png 	new file:   game/2048_chaodai/meta/icon_bitcoin.svg 	new file:   game/2048_chaodai/meta/icon_pp.svg 	new file:   game/2048_chaodai/style/main.css

---
+++ game/2048_chaodai/js/tile.js
@@ -0,0 +1,32 @@
+function Tile(position, value) {
+  this.name_change = ["夏", "商", "周", "春秋战国", "秦汉", "三国", "魏晋南北朝", "隋唐", "宋", "元明", "清"]
+  this.x = position.x;
+  this.y = position.y;
+  this.true_value = value || 2;
+  this.value = this.name_change[Math.log(this.true_value) / Math.log(2) - 1];
+
+  this.previousPosition = null;
+  this.mergedFrom = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = {
+    x: this.x,
+    y: this.y
+  };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
+
+Tile.prototype.serialize = function () {
+  return {
+    position: {
+      x: this.x,
+      y: this.y
+    },
+    value: this.true_value
+  };
+};
